90babd541fe41e6b9fd158dc337503598b981ddf
koe <ukoe@protonmail.com>
2020-12-26 11:29:33 -0600

    add missing files

---
+++ Sources/AsyncTokens/token_process_intermediary.h
@@ -0,0 +1,104 @@
+// token process intermediary base class
+
+#ifndef TOKEN_PROCESS_INTERMEDIARY_765678987_H
+#define TOKEN_PROCESS_INTERMEDIARY_765678987_H
+
+//local headers
+#include "token_batch_generator.h"
+#include "token_batch_consumer.h"
+
+//third party headers
+
+//standard headers
+#include <memory>
+#include <vector>
+
+
+/// designed to stand between two AsyncTokenProcesses
+/// it consumes the first process's outputs, then creates a new batch for the second process
+template <typename InTokenT, typename OutTokenT, typename FinalResultT>
+class TokenProcessIntermediary : public TokenBatchConsumer<InTokenT, FinalResultT>, public TokenBatchGenerator<OutTokenT>
+{
+//member types
+public:
+	using ConsumerT = TokenBatchConsumer<InTokenT, FinalResultT>;
+	using GeneratorT = TokenBatchGenerator<OutTokenT>;
+
+//constructors
+	/// default constructor: disabled
+	TokenProcessIntermediary() = delete;
+
+	/// normal constructor
+	TokenProcessIntermediary(const int consumer_batch_size,
+			const int generator_batch_size,
+			const int max_shuttle_queue_size) :
+		ConsumerT{consumer_batch_size},
+		GeneratorT{generator_batch_size},
+		m_shuttle_queue{max_shuttle_queue_size}
+	{}
+
+	/// copy constructor: disabled
+	TokenProcessIntermediary(const TokenProcessIntermediary&) = delete;
+
+//destructor: default
+	virtual ~TokenProcessIntermediary() = default;
+
+//overloaded operators
+	/// asignment operator: disabled
+	TokenProcessIntermediary& operator=(const TokenProcessIntermediary&) = delete;
+	TokenProcessIntermediary& operator=(const TokenProcessIntermediary&) const = delete;
+
+//member functions
+	/// consume a token from first process; should call AddNextBatch() periodically
+	virtual void ConsumeToken(std::unique_ptr<InTokenT> input_token, const std::size_t index_in_batch) = 0;
+
+	/// get final result; should also reset the child object in case a new production run is started
+	virtual std::unique_ptr<FinalResultT> GetFinalResultImpl() = 0;
+
+	/// obligatory override for TokenBatchGenerator
+	virtual void ResetGenerator() override {}
+
+	/// wrapper for getting the final result
+	virtual std::unique_ptr<FinalResultT> GetFinalResult() override
+	{
+		// shut down the shuttle queue (GetFinalResult() should be called after the last ConsumeToken())
+		m_shuttle_queue.ShutDown();
+
+		return GetFinalResultImpl();
+	}
+
+	/// add next token set to queue for second process to obtain (blocks)
+	void AddNextBatch(std::vector<std::unique_ptr<OutTokenT>> &out_batch)
+	{
+		m_shuttle_queue.InsertToken(out_batch);
+	}
+
+	/// gets a token set when it is available (blocks)
+	virtual bool GetTokenSet(std::vector<std::unique_ptr<TokenT>> &return_token_set) override
+	{
+		return (m_shuttle_queue.GetToken(return_token_set));
+	}
+
+private:
+//member variables
+	/// queue for passing batch of OutTokens to next process
+	TokenQueue<std::vector<std::unique_ptr<OutTokenT>>> m_shuttle_queue{};
+};
+
+
+#endif //header guard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sources/cv_vid_frames_generator.h
@@ -0,0 +1,160 @@
+// implementation of TokenBatchGenerator for generating fragmented opencv video frames
+
+#ifndef CV_VID_FRAMES_GENERATOR_765678987_H
+#define CV_VID_FRAMES_GENERATOR_765678987_H
+
+//local headers
+#include "token_batch_generator.h"
+#include "cv_util.h"
+
+//third party headers
+#include <opencv2/opencv.hpp>	//for video manipulation (mainly)
+
+//standard headers
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+
+/// derive from this class with implementation of 'result handling'
+/// extracts frames from a cv::VideoCapture and breaks them into chunks for tokenized batched processing
+/// assumes pixels are defined with unsigned chars
+class CvVidFramesGenerator final : public TokenBatchGenerator<typename cv::Mat>
+{
+//member types
+public:
+	using TokenT = typename cv::Mat;
+	using ParentT = TokenBatchGenerator<TokenT>;
+
+//constructors
+	/// default constructor: disabled
+	CvVidFramesGenerator() = delete;
+
+	/// normal constructor
+	CvVidFramesGenerator(cv::VideoCapture &vid,
+			const int horizontal_buffer_pixels,
+			const int vertical_buffer_pixels,
+			const long long frame_limit,
+			const bool use_grayscale,
+			const int batch_size) : 
+		ParentT{batch_size},
+		m_vid{vid},
+		m_frame_limit{frame_limit},
+		m_convert_to_grayscale{use_grayscale},
+		m_horizontal_buffer_pixels{horizontal_buffer_pixels},
+		m_vertical_buffer_pixels{vertical_buffer_pixels},
+		m_frame_width{m_vid.get(cv::CAP_PROP_FRAME_WIDTH)},
+		m_frame_height{m_vid.get(cv::CAP_PROP_FRAME_HEIGHT)}
+	{
+		// sanity checks
+		assert(m_vid.isOpened());
+		assert(m_horizontal_buffer_pixels >= 0);
+		assert(m_vertical_buffer_pixels >= 0);
+		assert(m_frame_width > 0);
+		assert(m_frame_height > 0);
+		// make sure the Mats obtained will be composed of unsigned chars
+		// http://ninghang.blogspot.com/2012/11/list-of-mat-type-in-opencv.html
+		auto format{m_vid.get(cv::CAP_PROP_FORMAT)};
+		assert(format == CV_8UC1 ||
+				format == CV_8UC2 ||
+				format == CV_8UC3 || 
+				format == CV_8UC4);
+
+		// interpet video frames as RGB format for consistency
+		vid.set(cv::CAP_PROP_CONVERT_RGB, true);
+	}
+
+	/// copy constructor: disabled
+	CvVidFramesGenerator(const CvVidFramesGenerator&) = delete;
+
+	/// destructor
+	virtual ~CvVidFramesGenerator() = default;
+
+//overloaded operators
+	/// asignment operator: disabled
+	CvVidFramesGenerator& operator=(const CvVidFramesGenerator&) = delete;
+	CvVidFramesGenerator& operator=(const CvVidFramesGenerator&) const = delete;
+
+//member functions
+	/// get token set from generator (set of frame segments)
+	virtual bool GetTokenSet(std::vector<std::unique_ptr<TokenT>> &return_token_set) override
+	{
+		cv::Mat frame{};
+
+		// leave if reached frame limit
+		if (m_frame_limit > 0 && m_frame_limit <= m_frames_consumed)
+			return false;
+
+		// get next frame from video
+		m_vid >> frame;
+
+		// leave if reached the end of the video or frame is corrupted
+		if (!frame.data || frame.empty())
+			return false;
+
+		// convert to grayscale if desired
+		// this should always work since the vid's CAP_PROP_CONVERT_RGB property was set
+		if (m_convert_to_grayscale)
+		{
+			cv::cvtColor(frame, frame, cv::COLOR_RGB2GRAY);
+		}
+
+		// break frame into chunks
+		return_token_set.clear();		//expectation of cv_mat_to_chunks()
+
+		if (!cv_mat_to_chunks(frame, return_token_set, 1, static_cast<int>(GetBatchSize()), m_horizontal_buffer_pixels, m_vertical_buffer_pixels))
+			std::cerr << "Breaking frame (" << m_frames_consumed + 1 << ") into chunks failed unexpectedly!\n";
+
+		m_frames_consumed++;
+
+		return true;
+	}
+
+	/// reset in case the token process is restarted
+	virtual void ResetGenerator() override
+	{
+		// point video back to first frame
+		m_vid.set(cv::CAP_PROP_FRAME_COUNT, 0);
+
+		m_frames_consumed = 0;
+	}
+
+private:
+//member variables
+	/// horizontal buffer (pixels) on edge of each chunk (overlap region)
+	const int m_horizontal_buffer_pixels{};
+	/// vertical buffer (pixels) on edge of each chunk (overlap region)
+	const int m_vertical_buffer_pixels{};
+	/// width of each frame (pixels)
+	const int m_frame_width{};
+	/// height of each frame (pixels)
+	const int m_frame_height{};
+
+	/// opencv video to get frames from
+	cv::VideoCapture m_vid{};
+	/// max number of frames to process (negative means go until end of vid)
+	const long long m_frame_limit{};
+	/// if frames should be converted to grayscale before being tokenized
+	const bool m_convert_to_grayscale{false};
+
+	/// frame counter
+	long long m_frames_consumed{0};
+};
+
+
+#endif //header guard
+
+
+
+
+
+
+
+
+
+
+
+
+
