975018409a5e91824846402d5cbb14e357a1492a
koe <ukoe@protonmail.com>
2021-02-07 11:05:38 -0600

    bump version to v0.6.0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def get_cmake_args():
 
 setup(
     name="cvvidproc",
-    version="0.5.4",
+    version="0.6.0",
     description="C++ bindings for multithreaded opencv video processing",
     author="koe",
     license="MIT",
