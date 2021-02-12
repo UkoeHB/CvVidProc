d70da184ba9797d1b949cc6f142dbf0dd1b45dff
koe <ukoe@protonmail.com>
2021-02-12 15:49:07 -0600

    update python bindings for TrackBubbles

--- PySources/cvvidproc/__init__.py
+++ PySources/cvvidproc/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # thanks to: https://github.com/pybind/scikit_build_example
 
-from ._core import __doc__, WorkerThreadsFromMax, VidBgPack, GetVideoBackground
+from ._core import __doc__, VidBgPack, GetVideoBackground, HighlightBubblesPack, AssignBubblesPack, VidBubbleTrackPack, TrackBubbles
