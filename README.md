94a6e19b37ede81f0e738b6d54a3eadf1382704a
UkoeHB <ukoe@protonmail.com>
2021-02-27 17:49:51 -0600

    fix bug with finding atomic lib; v0.9.2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # README
 
 <!-- must update setup.py version as well -->
-version 0.9.1
+version 0.9.2
 
 Dependencies:
 
--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def get_cmake_args():
 
 setup(
     name="cvvidproc",
-    version="0.9.1",
+    version="0.9.2",
     description="C++ bindings for multithreaded opencv video processing",
     author="koe",
     license="MIT",
